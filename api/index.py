acbdef7cc2857d6a1b8d72f858826006818d1a04
Mehmet-Adil <142682144+Mehmet-Adil@users.noreply.github.com>
2025-11-29 05:14:12 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,10 @@
 # api/index.py
-from .app import app as handler
-
+from app import app
+handler = app
 # This file only imports the Flask app instance named 'app' from app.py
 
 # and exposes it as 'application' for Vercel's build system.
+
 
 
 
