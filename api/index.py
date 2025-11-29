4534d89404e7589b9d21d7e384214b3352b4aea3
Mehmet-Adil <142682144+Mehmet-Adil@users.noreply.github.com>
2025-11-29 03:26:28 +0100

    Remove alias for Flask app import

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 # api/index.py
-from .app import app as application
+from .app import app
 
 # This file only imports the Flask app instance named 'app' from app.py
 
 # and exposes it as 'application' for Vercel's build system.
+
