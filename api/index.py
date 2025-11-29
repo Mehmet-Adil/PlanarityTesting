3811c52a7f3375ac29a4a0cc2777b8186b719e1e
Mehmet-Adil <142682144+Mehmet-Adil@users.noreply.github.com>
2025-11-29 03:24:15 +0100

    Fix import statement for Flask app in index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 # api/index.py
-from app import app as application
+from .app import app as application
 
 # This file only imports the Flask app instance named 'app' from app.py
+
 # and exposes it as 'application' for Vercel's build system.
